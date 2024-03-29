ce4077bcf6b565bf86870a0b638ed9e85324c07e
shreya6323 <124079028+shreya6323@users.noreply.github.com>
2024-03-29 08:59:23 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,27 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// module.exports = function(app) {
+//     app.use(
+//         '/api',
+//         createProxyMiddleware({
+//             target: 'http://localhost:8080',  // Update the port if necessary
+//             changeOrigin: true,
+          
+//         })
+//     );
+// };
+
+
+
+// No longer need http-proxy-middleware
+const request = require('request');
 module.exports = function(app) {
     app.use(
         '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:8080',  // Update the port if necessary
-            changeOrigin: true,
-          
-        })
+        (req, res) => {
+            // Replace 'https://clean-pick.vercel.app' with your actual Vercel deployment URL
+            const targetUrl = 'https://clean-pick.vercel.app'; 
+            req.pipe(request({ url: targetUrl + req.url })).pipe(res);
+        }
     );
 };
-
-
-
