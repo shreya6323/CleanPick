e5b06827e16e64d7ee4c6d79ac757faf6ae9ae35
shreya6323 <124079028+shreya6323@users.noreply.github.com>
2024-03-29 09:11:13 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,27 +1,27 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//     app.use(
-//         '/api',
-//         createProxyMiddleware({
-//             target: 'http://localhost:8080',  // Update the port if necessary
-//             changeOrigin: true,
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:8080',  // Update the port if necessary
+            changeOrigin: true,
           
-//         })
-//     );
-// };
+        })
+    );
+};
 
 
 
 // No longer need http-proxy-middleware
-const request = require('request');
-module.exports = function(app) {
-    app.use(
-        '/api',
-        (req, res) => {
-            // Replace 'https://clean-pick.vercel.app' with your actual Vercel deployment URL
-            const targetUrl = 'https://clean-pick-o32sb6yzv-shreya6323s-projects.vercel.app/'; 
-            req.pipe(request({ url: targetUrl + req.url })).pipe(res);
-        }
-    );
-};
+// const request = require('request');
+// module.exports = function(app) {
+//     app.use(
+//         '/api',
+//         (req, res) => {
+//             // Replace 'https://clean-pick.vercel.app' with your actual Vercel deployment URL
+//             const targetUrl = 'https://clean-pick-o32sb6yzv-shreya6323s-projects.vercel.app/'; 
+//             req.pipe(request({ url: targetUrl + req.url })).pipe(res);
+//         }
+//     );
+// };
